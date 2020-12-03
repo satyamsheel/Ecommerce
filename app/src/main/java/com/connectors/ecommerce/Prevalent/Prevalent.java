ed14829165fb507034562997444c3715b22c9de6
satyam sheel <sheelsatyam@ymail.com>
2020-12-03 21:47:20 +0530

    Initial commit

---
+++ app/src/main/java/com/connectors/ecommerce/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.connectors.ecommerce.Prevalent;
+
+import com.connectors.ecommerce.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
